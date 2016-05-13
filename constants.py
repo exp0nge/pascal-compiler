10e2af45ad7c3e159950f993101e89b5db1a4959
exp0nge <mdislamwork@gmail.com>
2016-05-13 13:31:15 -0400

    Updated op code constants

--- constants.py
+++ constants.py
@@ -16,8 +16,26 @@ class OPCODE(object):
     DIV:
     CVR:
     """
-    PUSHI, PUSH, POP, ADD, SUB, MULTIPLY, DIVIDE, DIV, CVR, GTR, DUP = range(11)
+    PUSHI = 0
+    PUSH = 1
+    POP = 2
+    ADD = 3
+    SUB = 4
+    MULTIPLY = 5
+    DIVIDE = 6
+    DIV = 7
+    CVR = 8
+    GTR = 9
+    DUP = 10
+    JMP = 11
+    JFALSE = 12
+    JTRUE = 13
+    HALT = 14
+    PRINT_I = 15
+    PRINT_C = 16
+    PRINT_B = 17
+    PRINT_R = 18
 
 
 class TYPE(object):
-    I, R, B, C = range(4)
+    I, R, B, C, S = range(5)
